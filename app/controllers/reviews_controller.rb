2c1a30ae5c5e170f12e4ab75201c172923482521
paulderuelle <p.deruelle.mail@gmail.com>
2023-06-27 16:40:44 +0200

    some reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,42 @@
 class ReviewsController < ApplicationController
+  before_action :set_event, only: %i[new create edit update]
+  before_action :set_review, only: %i[edit update]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to event_path(@event), notice: 'Review was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to event_path(@event), notice: 'Review was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :user_id, :event_id)
+  end
 end
