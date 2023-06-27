9a8be02ee8ab1c357744e4021b36e64a8a71a9dd
DENEUFVE Maximilien <maxdeneufve@gmail.com>
2023-06-27 15:47:13 +0200

    booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,55 @@
 class BookingsController < ApplicationController
+  before_action :set_event, only: %i[new create]
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.event = @event
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render "events/show", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.status == "accepted" || @booking.status == "rejected"
+      if @booking.save
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:comment, :status)
+  end
 end
