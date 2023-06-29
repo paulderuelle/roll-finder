875c561a36c411dd6afe782c381187e37ff96ded
Edouard Ravelomanantsoa <edravelo@gmail.com>
2023-06-29 17:34:32 +0200

    map first upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_101324) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_140709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +105,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_101324) do
     t.datetime "updated_at", null: false
     t.datetime "start_hours"
     t.datetime "end_hours"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
