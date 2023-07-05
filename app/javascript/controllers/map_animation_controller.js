925580eba6d71e7c7a956901920f250055da6b53
Edouard Ravelomanantsoa <edravelo@gmail.com>
2023-07-05 15:20:08 +0200

    seachbar event hide and show

--- app/javascript/controllers/map_animation_controller.js
+++ app/javascript/controllers/map_animation_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map-animation"
 export default class extends Controller {
-  static targets = [ "wrapper", "map", "button" ];
+  static targets = [ "wrapper", "map", "button", "searchbar", "cards" ];
   static mapOpened;
 
   connect() {
@@ -21,10 +21,14 @@ export default class extends Controller {
     this.mapOpened = true;
     this.wrapperTarget.classList.remove("slideOut");
     this.mapTarget.classList.remove("fadeOut");
+    this.mapTarget.classList.add("fadeIn");
     this.buttonTarget.classList.remove("slideUp");
     this.wrapperTarget.classList.add("slideIn");
-    this.mapTarget.classList.add("fadeIn");
     this.buttonTarget.classList.add("slideDown");
+    this.searchbarTarget.classList.add("disapear");
+    this.searchbarTarget.classList.remove("apear");
+    this.cardsTarget.classList.add("cardsUp");
+    this.cardsTarget.classList.remove("cardsDown");
     this.buttonTarget.innerHTML = "Show events <i class='fa-solid fa-chevron-up'></i>";
   }
 
@@ -36,6 +40,10 @@ export default class extends Controller {
     this.wrapperTarget.classList.add("slideOut");
     this.mapTarget.classList.add("fadeOut");
     this.buttonTarget.classList.add("slideUp");
+    this.searchbarTarget.classList.add("apear");
+    this.searchbarTarget.classList.remove("disapear");
+    this.cardsTarget.classList.remove("cardsUp");
+    this.cardsTarget.classList.add("cardsDown");
     this.buttonTarget.innerHTML = "Show the map <i class='fa-solid fa-chevron-down'></i>";
   }
 }
