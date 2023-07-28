8e00fd12d7c6540a0fbb3cfec193c2a67b71d4b5
paulderuelle <p.deruelle.mail@gmail.com>
2023-07-28 22:48:44 +0200

    add scanning gemfile

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -4,15 +4,29 @@ class GithubController < ApplicationController
   def index
     @link = "https://github.com/login/oauth/authorize?client_id=#{CLIENT_ID}"
   end
+
   def callback
     code = params["code"]
+
     token_data = exchange_code(code)
+
     if token_data.key?("access_token")
       token = token_data["access_token"]
+
       user_info = user_info(token)
       @handle = user_info["login"]
       @name = user_info["name"]
-      @render = "Successfully authorized! Welcome, #{@name} (#{@handle})."
+
+      repositories_with_gemfile = user_repositories_with_gemfile(token)
+      @repositories_with_gemfile = repositories_with_gemfile
+
+      # repositories = user_repositories(token)
+
+      # @render = "Successfully authorized! Welcome, #{@name} (#{@handle})."
+      # repositories.each do |repo|
+      #   @render += repo['name'].to_s
+      # end
+
     else
       @render = "Authorized, but unable to exchange code #{code} for token."
     end
